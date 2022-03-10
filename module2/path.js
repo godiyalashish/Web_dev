57e86a24cfa717d95e4b876ea9ded87fbb53e8b7
godiyalashish <ashishgodiyal.333@gmail.com>
2022-03-10 22:10:41 +0530

    code

---
+++ module2/path.js
@@ -0,0 +1,9 @@
+//E:\FJP 6\module2
+
+let path = require("path");
+let extensionName = path.extname("E:\FJP 6\module2\intro.js");
+console.log(extensionName);
+
+let baseName = path.basename("E:\FJP 6\module1\index.html");
+console.log(__dirname);
+console.log(__filename);
