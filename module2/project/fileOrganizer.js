9d3e5fcb9fcce2d6f98cfbb462c6013d78983f07
godiyalashish <ashishgodiyal.333@gmail.com>
2022-03-23 13:40:25 +0530

    fileOrganizer code

--- module2/project/fileOrganizer.js
+++ module2/project/fileOrganizer.js
@@ -1,2 +1,51 @@
-let input  = process.argv[2];
-console.log(input);
+let fs = require("fs");
+let path = require("path");
+let folderPath  = process.argv[2];
+let folderExists = fs.existsSync(folderPath);
+let extenstions = {
+    Audio:[".mp3"],
+    Video:[".mp4",".mkv"],
+    Document:[".doc", ".xlsx", ".pdf"],
+    Image:[".jpg", ".jpeg", ".png",".gif"],
+    Software:[".exe"]
+};
+
+if(folderPath){
+    // console.log("path is valid");
+    let files = fs.readdirSync(folderPath);
+    for(let i=0; i<files.length; i++){
+        let ext = path.extname(files[i])
+        let folderName = giveFolderName(ext);
+        let pathOfFolder = path.join(folderPath,folderName);
+        let exists = fs.existsSync(pathOfFolder);
+        if(exists){
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }else{
+            fs.mkdirSync(pathOfFolder);
+            moveFile(folderPath,pathOfFolder,files[i]);
+        }
+    }
+}else{
+    console.log("path not valid");
+}
+
+function giveFolderName(ext){
+    for(let key in extenstions){
+        let extArr = extenstions[key];
+        for(let i=0; i<extArr.length; i++){
+            if(ext ==extArr[i]){
+                return key;
+            }      
+        }
+    }
+    return 'Other';
+    
+}
+
+function moveFile(folderPath, pathOfFolder, fileName){
+    let source = path.join(folderPath, fileName);
+    let destination = path.join(pathOfFolder, fileName);
+    fs.copyFileSync(source,destination);
+    fs.unlinkSync(source);
+
+}
